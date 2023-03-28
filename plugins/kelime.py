3475143c379472ed19b8f19ae8e9728ff4800436
KizHatce <129085642+KizHatce@users.noreply.github.com>
2023-03-29 00:42:16 +0300

    Update kelime.py

--- plugins/kelime.py
+++ plugins/kelime.py
@@ -4,24 +4,24 @@ import random
 
 async def dongu(bot, message, sayi, say):
     say +=1
-    tedilen = await bot.ask(message.from_user.id, "Yanlış Cevap!")
+    tedilen = await bot.ask("Yanlış Cevap!")
     if int(tedilen.text) == sayi:
         await message.reply_text(f"Tebrikler {say} deneyişte doğru cevabı buldun..")
     else:
         await dongu(bot, message, sayi, say)
 
 async def sayitahmin(bot, message, sayi, say):
-    tedilen = await bot.ask(message.from_user.id, "aklımdan bir sayı tuttum bul bakalım..")
+    tedilen = await bot.ask("aklımdan bir sayı tuttum bul bakalım..")
     say +=1
     if int(tedilen.text) == sayi:
-        await message.reply_text(f"Tebrikler {say} deneyişte doğru cevabı buldun..")
+        await message.reply_text(f"Tebrikler **{say}** deneyişte doğru cevabı buldun..")
     else:
         await dongu(bot, message, sayi, say)
 
 @Client.on_message(filters.command('sayi'))
 async def sayioyunu(bot, message):
     try:
-        sayi = random.randint(1, 10) # 1 ile 1000 arasından bir sayı seçtiriyorum.
+        sayi = random.randint(1, 100) # 1 ile 1000 arasından bir sayı seçtiriyorum.
         say = 0
         await sayitahmin(bot, message, sayi, say)
     except Exception as e:
