59c21d5f69e27f3502cc85815063d5b807014c73
KizHatce <129085642+KizHatce@users.noreply.github.com>
2023-03-29 00:20:14 +0300

    Create kelime.py

---
+++ plugins/kelime.py
@@ -0,0 +1,28 @@
+from pyrogram import Client, filters
+from pyromod import listen  
+import random
+
+async def dongu(bot, message, sayi, say):
+    say +=1
+    tedilen = await bot.ask("Yanlış Cevap!")
+    if int(tedilen.text) == sayi:
+        await message.reply_text(f"Tebrikler {say} deneyişte doğru cevabı buldun..")
+    else:
+        await dongu(bot, message, sayi, say)
+
+async def sayitahmin(bot, message, sayi, say):
+    tedilen = await bot.ask("aklımdan bir sayı tuttum bul bakalım..")
+    say +=1
+    if int(tedilen.text) == sayi:
+        await message.reply_text(f"Tebrikler {say} deneyişte doğru cevabı buldun..")
+    else:
+        await dongu(bot, message, sayi, say)
+
+@Client.on_message(filters.command('sayi'))
+async def sayioyunu(bot, message):
+    try:
+        sayi = random.randint(1, 10) # 1 ile 1000 arasından bir sayı seçtiriyorum.
+        say = 0
+        await sayitahmin(bot, message, sayi, say)
+    except Exception as e:
+        await message.reply_text(e)
