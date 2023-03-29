95f6bdac23dac9754564eb547821838225bc6456
KizHatce <129085642+KizHatce@users.noreply.github.com>
2023-03-30 00:33:14 +0300

    Update hava.py

--- plugins/hava.py
+++ plugins/hava.py
@@ -43,9 +43,10 @@ async def fotogetir(bot, message):
         url = f"https://www.google.com/search?q={aranacak}+&prmd=imnv&sxsrf=APwXEde70utXdHW4TzxSkD4soDMpiDzMag:1680124608010&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiB-sDzh4L-AhXDcvEDHU7WBE4Q_AUIHigB&biw=360&bih=662&dpr=3"
         istek = requests.get(url)
         corba = BeautifulSoup(istek.content, "lxml")
-        fotourl = corba.findAll("img")
+        fotourl = corba.findAll('div', class_='NZWO1b')
         for foto in fotourl:
-            text = foto.get("alt")
+            text1 = foto.find("img")
+            text = text1.get("alt")
             await message.reply_text(text)
         LOGGER.info(corba)
     except Exception as e:
