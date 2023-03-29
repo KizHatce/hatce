bc97e6120062573b7a41c57545c53144ccd543ea
KizHatce <129085642+KizHatce@users.noreply.github.com>
2023-03-29 23:44:34 +0300

    Create hava.py

---
+++ plugins/hava.py
@@ -0,0 +1,28 @@
+from pyrogram import Client, filters
+import requests
+from bs4 import BeautifulSoup
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+                    level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+@Client.on_message(filters.command('hava'))
+async def havabilgisi(bot, message):
+    try:
+        mes = message.text.split(" ")
+        if len(mes) == 3:
+            il = mes[1]
+            ilce = mes[2]
+        elif len(mes) == 2:
+            il = mes[1]
+            ilce = mes[1]
+        elif len(mes) == 1:
+            await message.reply_text("Hatalı Kullanım Şunu dene:\n\n`/hava İstanbul Avcılar`")
+            return
+        url = f"https://www.google.com/search?&q={il}+{ilce}+hava+durumu&lr=lang_tr&hl=tr"
+        istek = requests.get(url)
+        corba = BeautifulSoup(istek.content, "lxml")
+        LOGGER.info(corba)
+    except Exception as e:
+        await message.reply_text(e)
