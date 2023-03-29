21117279d046c1babb479e8490c2c0edb765afea
KizHatce <129085642+KizHatce@users.noreply.github.com>
2023-03-30 00:57:36 +0300

    Update hava.py

--- plugins/hava.py
+++ plugins/hava.py
@@ -40,7 +40,7 @@ async def fotogetir(bot, message):
     try:
         aranacaktemp = message.text.split(" ", 1)[1]
         aranacak = aranacaktemp.replace(" ", "+")
-        url = f"https://yandex.com.tr/gorsel/touch/search?text={aranacak}&source=tabbar"
+        url = f"https://www.google.com/search?q={aranacak}+&prmd=imnv&sxsrf=APwXEde70utXdHW4TzxSkD4soDMpiDzMag:1680124608010&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiB-sDzh4L-AhXDcvEDHU7WBE4Q_AUIHigB&biw=360&bih=662&dpr=3"
         istek = requests.get(url)
         corba = BeautifulSoup(istek.content, "lxml")
         LOGGER.info(corba)
