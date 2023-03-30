07bf7c1643a885b67001cc4216e196c110439dfe
KizHatce <129085642+KizHatce@users.noreply.github.com>
2023-03-30 21:22:24 +0300

    Update fotograf.py

--- plugins/fotograf.py
+++ plugins/fotograf.py
@@ -9,9 +9,9 @@ yaslar = ["57", "34", "55", "52"]
 async def fotooyunu(bot, message):
     try:
         dosya = random.randint(0, 3)
-        dir = "plugins/fotolar"
+        dir = "plugins/fotolar1"
         temp = os.listdir(dir)[dosya]
-        photo = f"plugins/fotolar/{temp}"
+        photo = f"plugins/fotolar1/{temp}"
         yas = yaslar[dosya]
         await message.reply_photo(
             photo=photo,
