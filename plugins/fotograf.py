c98ba0f0064c6404d365e8b6bd1e81cb0ca14820
KizHatce <129085642+KizHatce@users.noreply.github.com>
2023-03-30 20:52:01 +0300

    Update fotograf.py

--- plugins/fotograf.py
+++ plugins/fotograf.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 import random
 from pyromod import listen
+import os
 
 @Client.on_message(filters.command('foto'))
 async def fotooyunu(bot, message):
@@ -8,7 +9,7 @@ async def fotooyunu(bot, message):
         dosya = random.randint(1, 3)
         say = 1
         fotolar = "fotolar"
-        for photo in fotolar:
+        for photo in os.listdir(fotolar):
             if int(say) == dosya:
                 await message.reply_photo(photo)
             say +=1
