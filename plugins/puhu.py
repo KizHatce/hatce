198576875502d3be96fe5c74d089f2be346b0206
KizHatce <129085642+KizHatce@users.noreply.github.com>
2023-04-01 14:12:34 +0300

    Update puhu.py

--- plugins/puhu.py
+++ plugins/puhu.py
@@ -7,11 +7,16 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
+headers = {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
+          "content-type": "application/json",
+          } 
+
 @Client.on_message(filters.command('puhu'))
 async def puhutv(bot, message):
     try:
         url = "https://puhutv.com/fi-1-bolum-izle"
-        r = requests.get(url)
+        r = requests.get(url, headers=headers)
         c = BeautifulSoup(r.content, "lxml")
         LOGGER.info(c)
     except Exception as e:
