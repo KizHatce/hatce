e05df46772f9b7628ee5ef3c9012853a8b38c497
KizHatce <129085642+KizHatce@users.noreply.github.com>
2023-04-02 14:33:33 +0300

    Update puhu.py

--- plugins/puhu.py
+++ plugins/puhu.py
@@ -28,9 +28,13 @@ async def aldinaldingelecekhafta(bot, message):
         for foto in afislerr:
             photo = foto.get('src')
             fotolar.append(photo)
-        return fotolar
+        for brosur in fotolar:
+            await bot.send_photo(
+                chat_id = message.from_user.id,
+                photo = brosur)
+        await bot.send_message(message.from_user.id, "Aldın Aldın Gelecek Hafta Broşürleri Başarıyla Getirildi..") 
     except Exception as e:
-        await message.reply_text(e)
+        await bot.send_message(message.from_user.id, e)
 
 async def aldinaldinbuhafta(bot, message):
     try:
@@ -46,13 +50,14 @@ async def aldinaldinbuhafta(bot, message):
             await bot.send_photo(
                 chat_id = message.from_user.id,
                 photo = brosur)
+        await bot.send_message(message.from_user.id, "Aldın Aldın Bu Hafta Broşürleri Başarıyla Getirildi..") 
     except Exception as e:
         await bot.send_message(message.from_user.id, e)
 
 @Client.on_message(filters.command('a101'))
 async def a101getir(bot, message):
     try:
-       text = f"Merhaba {message.from_user.mention},\nAşağıdaki butoblardan hangi afişleri istediğiniz seç 😊"
+       text = f"Merhaba {message.from_user.mention},\nAşağıdaki butonlardan hangi Broşürleri istediğini seç 😊"
        await bot.send_message(
            chat_id = message.chat.id,
            text = text,
@@ -61,7 +66,13 @@ async def a101getir(bot, message):
         await message.reply_text(e)
 
 @Client.on_callback_query(filters.regex('^buhaftaaldin$'))
-async def help_cb(bot, message):
+async def aldinaldinbu(bot, message):
     await message.answer("Bu Hafta Aldın Aldın Broşürleri Getiriliyor...",
                          show_alert=True)
     await aldinaldinbuhafta(bot, message)
+
+@Client.on_callback_query(filters.regex('^gelecekhaftaaldin$'))
+async def aldinaldingelecek(bot, message):
+    await message.answer("Gelecek Hafta Aldın Aldın Broşürleri Getiriliyor...",
+                         show_alert=True)
+    await aldinaldingelecekhafta(bot, message)
