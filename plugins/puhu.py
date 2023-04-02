a1ae85cd2c511d7e464f84352c86b5f8081e7e31
KizHatce <129085642+KizHatce@users.noreply.github.com>
2023-04-02 14:12:26 +0300

    Update puhu.py

--- plugins/puhu.py
+++ plugins/puhu.py
@@ -1,3 +1,5 @@
+# Coded by @mmagneto (:d) 
+
 from pyrogram import Client, filters
 import requests
 from bs4 import BeautifulSoup
@@ -7,8 +9,9 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-async def afisgetir(bot, message):
+async def aldinaldingelecekhafta(bot, message):
     try:
         url = "https://www.a101.com.tr/aldin-aldin-gelecek-hafta-brosuru"
         r = requests.get(url)
@@ -22,12 +25,35 @@ async def afisgetir(bot, message):
     except Exception as e:
         await message.reply_text(e)
 
+async def aldinaldinbuhafta(bot, message):
+    try:
+        url = "https://www.a101.com.tr/aldin-aldin-bu-hafta-brosuru"
+        r = requests.get(url)
+        c = BeautifulSoup(r.content, "lxml")
+        afislerr = c.findAll('img', attrs={"class":"image0"})
+        fotolar = []
+        for foto in afislerr:
+            photo = foto.get('src')
+            fotolar.append(photo)
+        return fotolar
+    except Exception as e:
+        await message.reply_text(e)
 
 @Client.on_message(filters.command('a101'))
 async def a101getir(bot, message):
     try:
-       afisler = await afisgetir(bot, message)
-       for photo in afisler:
-           await message.reply_photo(photo)
+       text = "Merhaba {message.from_user.mention},\nAşağıdaki butoblardan hangi afişleri istediğiniz seç 😊"
+       buton = [[
+           InlineKeyboardButton(f'A101 Bu Hafta Aldın Aldın', callback_data='buhaftaaldin'),
+           InlineKeyboardButton(f'A101 Gelecek Hafta Aldın Aldın', callback_data='gelecekhaftaaldin')
+           ],[
+           InlineKeyboardButton(f'A101 Bu Haftanın Yıldızları', callback_data='buhaftayildiz'),
+           InlineKeyboardButton(f'A101 Gelecek Haftanın Yıldızları', callback_data="gelecekhaftayildiz")
+           ]]
+       butonlar = InlineKeyboardMarkup(buton)
+       await bot.send_message(
+           chat_id = message.chat.id,
+           text = text,
+           reply_markup = butonlar)
     except Exception as e:
         await message.reply_text(e)
