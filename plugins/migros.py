4b1df40f5e2f31454fd00f33d46d3d4bad0fd677
KizHatce <129085642+KizHatce@users.noreply.github.com>
2023-04-02 16:37:06 +0300

    Create migros.py

---
+++ plugins/migros.py
@@ -0,0 +1,40 @@
+# Coded by @mmagneto (:d) 
+
+from pyrogram import Client, filters
+import requests
+from bs4 import BeautifulSoup
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
+                    level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+butonlar = InlineKeyboardMarkup([[
+           InlineKeyboardButton(f'Migros Güncel', callback_data='migrosg')]])
+
+async def migrosguncel(bot, message):
+    try:
+        url = "https://www.money.com.tr/migroskop-dijital"
+        r = requests.get(url)
+        c = BeautifulSoup(r.content, "lxml")
+        LOGGER.info(c)
+    except Exception as e:
+        await bot.send_message(message.from_user.id, e)
+
+@Client.on_message(filters.command('migros') & filters.private)
+async def migros(bot, message):
+    try:
+       text = f"Merhaba {message.from_user.mention},\nAşağıdaki butonlardan hangi Broşürleri istediğini seç 😊"
+       await bot.send_message(
+           chat_id = message.chat.id,
+           text = text,
+           reply_markup = butonlar)
+    except Exception as e:
+        await message.reply_text(e)
+
+@Client.on_callback_query(filters.regex('^migrosg$'))
+async def migrosguncelgetir(bot, message):
+    await message.answer("Bu Hafta'ki Migros Broşürü Getiriliyor...",
+                         show_alert=True)
+    await migrosguncel(bot, message)
